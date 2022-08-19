c0bd047e01b2ab201f12136a70717c65d57979b9
Anuj Soni <40125467+anuj4207@users.noreply.github.com>
2022-08-19 15:13:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const notFound = require('./middleware/not-found')
 const errorHandler = require('./middleware/error-handler')
 require('dotenv').config()
 app.use(express.json())
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Methods","POST,GET,PUT,DELETE" );
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Methods","POST,GET,PUT,DELETE" );
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/api/v1/blog',blogRoute)
 app.use(notFound)
 app.use(errorHandler)
